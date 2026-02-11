5225aff2506b3ab26572318140b54509a551cf69
suppabhinandan-lab <supp.abhinandan@gmail.com>
2026-02-11 14:07:34 +0530

    Added the code for parsing the stream data

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 
 const results=[];
 fs.createReadStream('./kepler_data.csv')
+.pipe(parse({
+    comment:"#",
+    columns:true
+}))
 .on('data',(data)=>{
     results.push(data);
 })
