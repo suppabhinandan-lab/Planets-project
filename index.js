ba1b5b24b6c0507665142b6c7b896ebf0a35d844
suppabhinandan-lab <supp.abhinandan@gmail.com>
2026-02-11 14:22:12 +0530

    Added the code for finding the habitable planets

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const {parse}=require('csv-parse');
 const fs = require('fs');
 
-const results=[];
+// const results=[];
+let habitablePlanets=[]
+
+function isPlanetHabitable(planet){
+return planet['koi_disposition']=== 'CONFIRMED' && 
+planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 &&
+planet['koi_prad'] < 1.6;
+}
 fs.createReadStream('./kepler_data.csv')
 .pipe(parse({
     comment:"#",
     columns:true
 }))
 .on('data',(data)=>{
-    results.push(data);
+    if(isPlanetHabitable(data)){
+    habitablePlanets.push(data);
+    }
 })
 .on('error',(error)=>{
     console.log(error);
 })
 .on('end',()=>{
-    console.log(results)
+    console.log(`${habitablePlanets.length} habitable planets`)
 })
